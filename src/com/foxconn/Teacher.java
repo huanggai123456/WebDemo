1b6b5178ad9f38879118478506c910ff1b35463a
huanggai <2413812412@qq.com>
2018-10-26 17:36:49 +0800

    新增老师类

---
+++ src/com/foxconn/Teacher.java
@@ -0,0 +1,20 @@
+package com.foxconn;
+
+public class Teacher {
+	
+	private String name;
+	private String age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+}
